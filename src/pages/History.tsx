d5a0b28c11e7131aecb1f713b89044b9776ddde9
josesouzaa <josee.souzaaa@gmail.com>
2022-08-08 19:42:31 -0300

    Página de histórico

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,3 +1,75 @@
+import { ReactNode } from 'react'
+
+import classNames from 'classnames'
+
+interface StatusProps {
+  statusColor: 'yellow' | 'red' | 'green'
+  children: ReactNode
+}
+
+function Status({ statusColor, children }: StatusProps) {
+  return (
+    <span
+      className={classNames(
+        'flex items-center gap-2 before:content-[""] before:w-2 before:h-2 before:rounded-full ',
+        {
+          'before:bg-brand-green-500': statusColor === 'green',
+          'before:bg-brand-red-500': statusColor === 'red',
+          'before:bg-brand-yellow-500': statusColor === 'yellow'
+        }
+      )}
+    >
+      {children}
+    </span>
+  )
+}
+
 export function History() {
-  return <div>History</div>
+  return (
+    <main className="flex-1 p-14 flex flex-col">
+      <h1 className="text-2xl text-brand-gray-100">Meu histórico</h1>
+
+      <div className="history-table">
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Iníco</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+  )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,26 @@ export function Home() {
           <input
             id="task"
             type="text"
+            list="tasks-suggestion"
             placeholder="Dê um nome para o seu projeto"
             className="bg-transparent h-10 border-b-2 border-brand-gray-500 px-2 text-brand-gray-100 flex-1 placeholder:text-gray-500 focus:shadow-none focus:border-brand-green-500"
           />
+
+          <datalist id="tasks-suggestion">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <input
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
             className="bg-transparent h-10 border-b-2 border-brand-gray-500 px-2 text-brand-gray-100 w-16 placeholder:text-gray-500 focus:shadow-none focus:border-brand-green-500"
           />
 
